085af3fd9b4493278e3091541c8994ab3780deaf
James Kirk <jameskirk@gmail.com>
2026-01-23 17:11:04 -0500

    add logging

--- src/main/kotlin/org/example/ecommerceexamplebackendkotlinkafka/order/OrderService.kt
+++ src/main/kotlin/org/example/ecommerceexamplebackendkotlinkafka/order/OrderService.kt
@@ -1,22 +1,31 @@
 package org.example.ecommerceexamplebackendkotlinkafka.order
 
 import org.springframework.stereotype.Service
+import org.slf4j.LoggerFactory
 
 @Service
 class OrderService(
     private val orderFromStoreRepository: OrderFromStoreRepository,
-    private val productRepository: ProductRepository
+    private val productRepository: ProductRepository,
 ) {
-    fun createOrder(order: OrderRequest): OrderFromStore = orderFromStoreRepository.save(OrderFromStore().apply {
-        customerEmail = order.customerEmail
-        cartItems = order.cartItems.map { item ->
-            CartItem().apply {
-                quantity = item.quantity
-                product =
-                    productRepository.findBySkuId(item.skuId)
-                        ?: throw ProductNotFoundException("Product not found: ${item.skuId}")
+    companion object {
+        private val logger = LoggerFactory.getLogger(OrderService::class.java)
+    }
 
-            }
-        }.toMutableList()
-    })
+    fun createOrder(order: OrderRequest): OrderFromStore {
+        val savedOrder =  orderFromStoreRepository.save(OrderFromStore().apply {
+            customerEmail = order.customerEmail
+            cartItems = order.cartItems.map { item ->
+                CartItem().apply {
+                    quantity = item.quantity
+                    product =
+                        productRepository.findBySkuId(item.skuId)
+                            ?: throw ProductNotFoundException("Product not found: ${item.skuId}")
+
+                }
+            }.toMutableList()
+        })
+        logger.info("Order id ${savedOrder.orderId} saved")
+        return savedOrder
+    }
 }
