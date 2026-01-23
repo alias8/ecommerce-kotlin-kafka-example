611e059432a3f4b7b72dde54080e11f295e816de
James Kirk <jameskirk@gmail.com>
2026-01-23 13:24:59 -0500

    controller

---
+++ src/main/kotlin/org/example/ecommerceexamplebackendkotlinkafka/order/OrderController.kt
@@ -0,0 +1,23 @@
+package org.example.ecommerceexamplebackendkotlinkafka.order
+
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.*
+
+@RestController
+@RequestMapping("/orders")
+class OrderController(private val orderService: OrderService) {
+
+    @PostMapping
+    fun createOrder(@RequestBody order: OrderRequest): ResponseEntity<Order> {
+        val createOrder = orderService.createOrder(order)
+        return ResponseEntity.status(HttpStatus.CREATED).body(createOrder)
+    }
+
+    @ExceptionHandler(ProductNotFoundException::class)
+    fun handleProductNotFound(e: ProductNotFoundException): ResponseEntity<String> {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(
+            e.message
+        )
+    }
+}
